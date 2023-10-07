f81e80966eee46b013a7f82fbfacb635b764c23c
Arley <wilmerarleyd@gmail.com>
2023-10-07 03:42:49 -0500

    Se realizan todos los cambios y el commit inicial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,129 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import { About } from "@/components/About"
+import { ProgressBar } from "@/components/ProgressBar"
+import { ContainerEducation } from "@/components/container-education";
+import { ContainerMain } from "@/components/container-main";
+import { ContainerMainText } from "@/components/container-main-text";
+import Image from 'next/image';
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { BsFiletypePhp, BsGit, BsFillDatabaseFill } from "react-icons/bs";
+import { MdDesignServices } from "react-icons/md";
+import { FaPython } from "react-icons/fa";
+import { AiFillGithub } from "react-icons/ai";
+import { BiSolidSelectMultiple } from "react-icons/bi";
+import { Skills } from "@/components/skills";
+import { Button } from "@/components/button";
+import { IconLink } from "@/components/icon-links";
 
-export default function Home() {
+const Index = () => {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-full flex bg-[#F0F0F6]">
+      <aside className="bg-white p-4 flex flex-col w-[450px] gap-4 items-center" >
+        <section className=" h-[300px] py-10 flex flex-col items-center text-xs">
+          <div className=" h-[200px] w-[200px] mr">
+            <Image className='rounded-full' src='/image/Imagen de WhatsApp 2023-10-06 a las 23.11.00_8cd37093.jpg' height={"200"} width={"200"} alt='Profile Image' />
+          </div>
+          <span className="font-bold text-xl">Daniela Monsalve</span>
+          <span className="text-lg text-gray-text">Python Backend Developer</span>
+        </section>
+        <div className="flex flex-col w-[300px] h-[126px]">
+          <About Title="Age:" Description="25" />
+          <About Title="Residence:" Description="COL" />
+          <About Title="Dev" Description="backend" />
+          <About Title="Addres:" Description="Colombia, Bello" />
         </div>
-      </div>
+        <section className="py-4 h-[200px] w-[300px]">
+          <span className="font-semibold text-xl"> Languages </span>
+          <ProgressBar Title="English" Percentage="100" />
+          <ProgressBar Title="Spanish" Percentage="100" />
+        </section>
+        <section className="h-[400px] w-[300px]">
+          <span className="font-semibold w-[220px] h-[1.5px] text-xl"> Programing Languages </span>
+          <ProgressBar Title="Golang" Percentage="100%" />
+          <ProgressBar Title="Python" Percentage="100%" />
+          <ProgressBar Title="Java" Percentage="90%" />
+          <ProgressBar Title="Javascript" Percentage="85%" />
+          <ProgressBar Title="CSS" Percentage="95%" />
+          <ProgressBar Title="HTML" Percentage="80%" />
+          <ProgressBar Title="PHP" Percentage="90%" />
+        </section>
+        <section className=" h-[180px] w-[300px] " >
+          <p className="mb-2 text-xl font-semibold">Extra Skills</p>
+          <Skills Description="Fast Api Framework" Icono={BiSolidSelectMultiple} />
+          <Skills Description="Docker" Icono={BiSolidSelectMultiple} />
+          <Skills Description="Automatic testing" Icono={BiSolidSelectMultiple} />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        </section>
+      </aside>
+      <main className=" flex flex-col w-full m-3 gap-4 " >
+        <section className="p-4  w-[1300px] h-[467px] items-center flex" >
+          <div className="flex flex-col items-center">
+            <span className="px-9 py-8 font-bold text-7xl">I'm Daniela Monsalve </span>
+            <div>
+              <span className="px-9 text-[#FFB400] font-bold text-7xl">Back-end</span>
+              <span className="py-8 font-bold text-7xl">Developer</span>
+            </div>
+            <span className="py-10 px-9 text-xl">I'm very grateful that you've taken the time to look at my CV.</span>
+            <Button Title="Hire Me ->" href="https://www.linkedin.com/in/ldaniela-monsalve" />
+          </div>
+          <div className="mr flex justify-end">
+            <Image className='' src='/image/woman bussiness.png' height={"290"} width={"280"} alt='Profile Image' />
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+        </section>
+        <section className="  w-[1300px] h-[600px]">
+          <ContainerMainText Title="My Knowledge" Description="Experienced Python Backend Developer with 3 years of expertise in designing, developing, and implementing scalable and robust backend systems. Skilled in utilizing a range of tools, frameworks, and languages to build RESTful APIs and manage data effectively. Proven track record of managing complex projects involving cross-functional teams and delivering high-quality solutions on time." />
+          <div className="grid-cols-3 grid grid-rows-2">
+            <ContainerMain Title="Web development" Description="Blog,E-Commerce" Icono={HiOutlineDesktopComputer} />
+            <ContainerMain Title="PHP" Description="Full Handle of frameworks" Icono={BsFiletypePhp} />
+            <ContainerMain Title="GIT" Description="Knoledge About Git-Flow" Icono={BsGit} />
+            <ContainerMain Title="UX/UI Designer" Description="Website Design" Icono={MdDesignServices} />
+            <ContainerMain Title="DB" Description="Design And Settings of Data Bases" Icono={BsFillDatabaseFill} />
+            <ContainerMain Title="Python" Description="Full Experience As A Python Develop" Icono={FaPython} />
+          </div>
+        </section>
+        <section className="font-serif  w-[1300px] h-[653px]">
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <ContainerMainText Title="Education" Description="Bachelor of Systems Engineering (expected graduation date: 2024)" />
+          <ContainerEducation TitleLeft="University of Antioquia" DescriptionLeft="Systems Engineering" TitleRight="Relevant coursework" DescriptionRight="Data Structures and Algorithms
+                    Object-Oriented Programming -
+                    Database Management Systems -
+                    Web Development - 
+                    Operating Systems -
+                    Software Engineering
+                "/>
+          <ContainerEducation TitleLeft=" " DescriptionLeft=" " TitleRight="Skills" DescriptionRight="• Redis (including pub/sub and caching)
+                  • RabbitMQ (including message queues and publish/subscribe)
+                  • GCP Pub/Sub (Google Cloud Platform's message broker)
+                  • Python (including asynchronous and parallel programming)
+                  • FastAPI
+                  • PostgreSQL
+                  • Docker
+                  • HTML/CSS
+                  • Git/GitHub
+                  • Java
+                  • JavaScript
+                  • Golang
+                  • gRPC with Python
+                  • PHP/Laravel Framework
+                  • Scrum and Agile
+                  " />
+          <ContainerEducation TitleLeft=" " DescriptionLeft=" " TitleRight="Soft Skills" DescriptionRight="• Strong communication and interpersonal skills
+                  • Ability to work collaboratively in cross-functional teams
+                  • Problem-solving skills and ability to handle complex tasks
+                  • Self-starter and ability to work independently
+                  " />
+
+        </section>
+      </main>
+      <aside className=" m-10 flex flex-col w-full m-3 gap-3">
+        <span className="flex justify-center font-bold text-2xl text-[#2B2B2B]" >Links</span>
+        <IconLink Icono={AiFillGithub} href="https://github.com/DanielaMGX" />
+      </aside>
+    </div>
   )
 }
+
+export default Index
